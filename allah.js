ba59e9b3eea9a071a68bd4568720f0aae8c3f113
arifkoyani <arifkoyani@gmail.com>
2025-03-01 00:08:51 +0500

    cnewcoed

---
+++ allah.js
@@ -0,0 +1,12 @@
+// const Name="arif";
+// console.log(Name)
+// class Name {
+//   constructor(name) {
+//     this.name = name;
+//   }
+//   showName() {
+//     console.log("this is your name:", this.name);
+//   }
+// }
+// const myname = new Name("arif hussain");
+// myname.showName();
--- allah.py
+++ allah.py
@@ -37,10 +37,222 @@
 # names_fruits=names+fruit
 # print(len(names_fruits))
 
-class Name:
-    def __setname__(self,name):
-        self.name=name
-p4=Name()
+# class Name:
+#     def __setname__(self,name):
+#         self.name=name
+# p4=Name()
 
-p4.__setname__("aarif")
-print(p4.name)
+# p4.__setname__("aarif")
+# print(p4.name)
+
+
+
+
+
+
+# name=88
+# age=88
+# print(id(name)==id(age))
+
+# names=["ali","khan","mubeen","karim"]
+# print( "khani" in names)
+# print(49>8 and 88>9)
+
+
+
+# class names_age:
+#     def __init__(self,name,age):
+#         self.name=name
+#         self.age=age
+
+# details=names_age("arif",88)
+# print(details.name,details.age)
+
+
+# name="aliza"
+# age=88
+# print(name+" "+str(age))
+
+
+
+# print(5+6)
+# print(5-6)
+# print(8%7)
+# print(5**4)
+
+# here address of x is not update beause , x is any interger so it is immutable , mutable elements dont changes it memory address if 
+# it create once ,, so in this case , 5+8 is execute  and allocit a new address and n0w x address is appointed to this new address , not that 
+# same addres in ehich x=5  now x is not more available;
+# x=5
+# print(id(x))
+# x+=8!
+# print(id(x))
+# print((4,4,44,4)==(4,"aliza",4,4))
+
+# if 9>=0:
+#     print("9 is greater thena and equal to 0")
+
+# print(9!=99)
+# name="arif";
+# print("z" not in  name)
+# # print("l" not in "aliza")
+# print("Arif" not in [4,4,4,4,44,4,4,54654,4,4,4,5,456,5,756,7,5,45,5,5,4,4,4,4,4,4,5,456,5,756,7,5,45,5,5,4,4,4,4,4,4,5,456,5,756,7,5,45,5,5,4,4,4,4,4,4,5,456,5,756,7,5,45,5,5,4,4,4,4,4,4,5,456,5,756,7,5,45,5,5,4,4,4,4,4,4,5,456,5,756,7,5,45,5,5,4,4,4,4,4,4,5,456,5,756,7,5,45,5,5,4,4,4,4,4,4,5,456,5,756,7,5,45,5,5,4,4,4,4,4,4,5,456,5,756,7,5,45,5,5,4,4,4,4,4,4,5,456,5,756,7,5,45,5,5,4,4,4,4,4,4,5,456,5,756,7,5,45,5,5,4,4,4,4,4,4,5,456,5,756,7,5,45,5,5,4,4,4,4,4,4,5,456,5,756,7,5,45,5,5,4,4,4,4,4,4,5,456,5,756,7,5,45,5,5,4,4,4,4,4,4,5,456,5,756,7,5,45,5,5,4,4,4,4,4,4,5,456,5,756,7,5,45,5,5,4,4,4,4,4,4,5,456,5,756,7,5,45,5,5,4,4,4,4,4,4,5,456,5,756,7,5,45,5,5,4,4,4,4,4,4,5,456,5,756,7,5,45,5,5,4,4,4,4,4,4,5,456,5,756,7,5,45,5,5,4,4,4,4,4,4,5,456,5,756,7,5,45,5,5,4,4,4,4,4,4,5,456,5,756,7,5,45,5,5,4,4,4,4,4,4,5,456,5,756,7,5,45,5,5,4,4,4,4,4,4,5,456,5,756,7,5,45,5,5,4,4,4,4,4,4,5,456,5,756,7,5,45,5,5,4,4,4,4,40,4,5,456,5,756,7,5,45,5,5,4,4,4,4,4,4,5,456,5,756,7,5,45,5,5,4,4,4,4,4,4,5,456,5,756,7,5,45,5,5,4,4,4,4,4,4,5,456,5,756,7,5,45,5,5,"Arif",4,4,4,4,4,4,5,456,5,756,7,5,45,5,5,4,4,4,4,4,4,5,456,5,756,7,5,45,5,5,4,4,4,4,4,4,5,456,5,756,7,5,45,5,5,4,4,4,4,4,4,5,456,5,756,7,5,45,5,5,4,47,4,4,4,4,5,456,5,756,7,5,45,5,5,4,4,4,4,4,4,5,456,5,756,7,5,45,5,5,4,4,4,4,4,4,5,456,5,756,7,5,45,5,5,4,4,4,4,4,4,5,456,5,756,7,5,45,5,5,4,4,4,4,4,4,5,456,5,756,7,5,45,5,5,4,4,4,4,4,4,5,456,5,756,7,5,45,5,5,4,4,4,4,4,4,5,456,5,756,7,5,45,5,5,4,4,4,4,4,4,5,456,5,756,7,5,45,5,5,4,4,4,4,4,4,5,456,5,756,7,5,45,5,5,4,4,4,4,4,4,5,456,5,756,7,5,45,5,5,4,4,4,4,4,4,5,456,5,756,7,5,45,5,5,4,4,4,4,4,4,5,456,5,756,7,5,45,5,5,4,4,4,4,4,4,5,456,5,756,7,5,45,5,5,4,4,4,4,4,4,5,456,5,756,7,5,45,5,5,4,4,4,4,4,4,5,456,5,756,7,5,45,5,5,4,4,4,4,4,4,5,456,5,756,79,5,45,5,5,4,4,4,4,4,4,5,456,5,756,7,5,45,5,5,4,94,4,4,4,4,5,456,5,756,78,5,45,5,5,4,4,4,4,4,4,5,456,5,756,7,5,45,5,5,4,4,4,4,4,4,5,456,5,756,7,5,45,5,5,4,4,4,4,4,4,5,456,5,756,7,5,45,5,5,4,4,4,4,4,4,5,456,5,756,70,5,45,5,5,4,4,4,4,4,4,5,456,5,756,7,5,45,5,5,4,4,4,4,4,4,5,456,5,756,7,5,45,5,5,4,4,4,4,4,4,5,456,5,756,7,5,45,5,5,4,4,4,4,4,4,5,456,5,756,7,5,45,5,5,4,4,4,4,4,4,5,456,5,756,7,5,45,5,5,4,4,4])
+
+# x=9
+# y=x
+# print(bin(x))
+# age=9
+# print(type(age))
+# price=8.44
+# print( type(price))
+
+# lana=7+5j
+# print(type(lana))
+
+
+
+# class family_names:
+#     def __init__ (self,name,age,family_name):
+#         self.name=name
+#         self.age=age
+#         self.family=family_name
+#     def show_name(self):
+#             print(self.name,self.age,self.family)
+
+# house_one=family_names("arif",88,"koyani house")
+# house_two=family_names("rahat",77,"barcha-house");
+# house_one.show_name()
+# house_two.show_name()
+
+
+# name='''thi is
+#  new name'''
+# print( id(name))
+# name+='''this is
+#  new name'''
+# print(id(name))
+
+
+# lists=["name",454,"khani",8,{"name":"arif"}]
+# print(id(lists))
+# lists[0]="allah"
+# print(id(lists))
+# print(lists[4]["name"])
+
+# my_first_tuple=("arif",87,(77,77,"hunza"),"khan",77)
+# print(my_first_tuple[2][2])
+
+# mydictory={
+#     "name":"arif",
+#     "country":"pakistan",
+#     "marks":{"maths":77,"biology":66,"village":67}
+# }
+
+# print(mydictory["marks"]["biology"])
+
+
+# names=["aliza","hussasin","mama"]
+# names.append("karim")
+# names.insert(1,"ya-allah")
+# print(names)
+
+# details={
+#     "name":"hussain",
+#     "age":"45",
+#     "city":"hunza",
+#     "marks":[45,465,4,45,4,4,45,4]
+# }
+
+# print(details["marks"][0])
+
+# mydetails=[4,4,4,444,{"age":55,"dp":"https://chatgpt.com/c/67c07b9e-4054-800f-a0e7-998f103b92ab","school":[45,45,45,45,46,4,64,4545,45,{"mysite":"https://www.youtube.com/watch?v=eaxU2Dmo8uw&list=PLjVLYmrlmjGcQfNj_SLlLV4Ytf39f8BF7&index=14"}]
+# }]
+
+# mydetails[4]["school"][9]["mysite"]="hunza"
+# print(mydetails[4]["school"][9]["mysite"])
+
+
+# my_set={4,55,644,456}
+# print( 55 not in my_set)
+# name=int(input("enter the no"))
+# namec=int(input("enter the no two"))
+# print("your sum is ",(name+namec))
+
+# a = 10
+# b = 20
+# result = eval("a - b")
+# print(result)  # Output: 200
+
+# data = {"name": "Arif", "age": 25, "city": "Gilgit"}
+# print(data["name"])  # Output: Arif
+# print(data.keys())
+# print(data.values())
+# print(data.items())
+
+
+# x=40
+# y=40
+# print(x is not y)
+
+
+
+
+
+# class name:
+#     def __init__(self,name):
+#         self.name=name
+#     def show_name(self):
+#         print("this is your name : ",self.name)
+        
+
+# myname=name("arif")
+# myname.show_name()
+
+
+
+# class Names:
+#     def __init__(self,friend_name,father_name,mother_name):
+#         self.friend_name=friend_name
+#         self.father_name=father_name
+#         self.mother_name=mother_name
+#     def show_mother_name(self):
+#         print("mother name is :",self.mother_name)
+#     def show_father_name(self):
+#         print("this is father name :",self.father_name)
+#     def show_friend_name(self):
+#         print("this is friend name :",self.friend_name)
+
+
+
+#     def update_name(self,friend_name=None,father_name=None,mother_name=None):
+#         if friend_name:
+#             self.friend_name=friend_name
+#         if father_name:
+#             self.father_name=friend_name
+#         if mother_name:
+#             self.mother_name=mother_name
+#         print("update name succcessfully")
+
+# all_Names=Names("shahzad","musa","bano")
+# all_Names.show_friend_name()
+# all_Names.update_name(friend_name="afaq hussain")
+# # all_Names.show_friend_name()
+
+
+# number_a=int(input("Enter the name here"))
+# number_b=int(input("Enter the name here"))
+# print(number_a+number_b)
+
+# sum=eval("4+9")
+# print(sum)
+
+
+
+
+
+# number=int(input("enter the number"))
+# if number%2==0:
+#     print("this is Even")
+# else:
+#     print("this is noT even")
+    
