f1dfacf373353508f96218d8f900d561e4bcfdc1
arifkoyani <arifkoyani@gmail.com>
2025-03-13 00:41:49 +0500

    newcodew

--- allah.py
+++ allah.py
@@ -828,11 +828,112 @@ import random
 # file=open("name.text","w")
 # for i in range(40000):
 #     file.write(f"This is my Num : {i}  \n ")
-numbers=["hussain","ali","karim","obaid","hussain","aliza"]
-def add_eye(name):
-    return name+"i"
-new_list=list(map(lambda x:x+"i",numbers))
-new_names=list(map(add_eye,numbers))
-print(new_names)
+# numbers=["hussain","ali","karim","obaid","hussain","aliza"]
+# ages=[4,4,54,45,456]
+# # def add_eye(name):
+# #     return name+"i"
+# # new_list=list(map(lambda x:x+"i",numbers))
+# # new_names=list(map(add_eye,numbers))
+# # print(new_names)
+# for i,k in zip(numbers,ages):
+#     print(f"this is name : {i} and age is : {k}")
     
 
+
+# from enum import Enum
+# class colors(Enum):
+#     Red=4
+#     yellow=5
+#     green=44
+#     blue=44
+
+# print(colors.yellow)
+
+
+
+
+
+
+
+# from enum import Enum
+# class Names(Enum):
+#     manager="karim"
+#     shopKeeper="ali"
+#     driver="karimi"
+#     owner="arifkoyani"
+# print("The Name of Driver :",Names.driver.value,end=" | ")
+# print(f"Owner Name : {Names.owner.value}")
+
+
+# # basic Unpacking
+# a,fname,lname,age=[44,"arif","hussain",45]
+# print(f"this is a : {a} firstName is : {fname} lastName : {lname} and age is : {age}")
+
+# ADVANCE PACKIG with *
+# a,b,*c=[4,5,6,7,8]
+# print(f"this is last three : {c}")
+# a,*b,c=[4,5,6,7,8]
+# print(f"this is last three : {b}")
+# *a,b,c=[4,5,6,7,8]
+# print(f"this is first three : {a}")
+
+
+# Names=["hussain","ali","khan","karim"]
+
+# newNames=list(map(lambda x:x+"i",Names))
+# print(newNames)
+
+
+
+
+
+# _,_,a=[4,5,6]
+# print(a)
+
+# data=[44,5,55,655]
+# a,b,c,d=data
+# print(a)
+
+# names=("arif","hussain",(44,"hunza"))
+# name,last_name,details=names
+
+
+# def all_names(*names):
+#     mynames=names
+#     print(mynames)
+# all_names("husasin","khan","mubeen","aliza")
+
+# list1=[1,2,3,4,5]
+# list2=[6,7,8,9,10]
+# combine=[*list1,*list2]
+# print(F"both combine numbers : {combine}")
+
+# dic1={"name":"arif","father":"khan","city":"hunza","age":88}
+# dic2={"name":"aliza","father":"muhammad","city":"karimabad","age":18}
+
+# merge_Dictionary={**dic1,**dic2}
+# print(f"this is merge dictionary :",merge_Dictionary)
+
+
+# x=44
+# y=45
+# print(x,y)
+
+# x,y=y,x
+# print(x,y)
+
+# with open("./allah.py","r") as file:
+#     aa=file.read()
+#     print(aa)
+
+# mu-name="arf"
+# print(mu-name)
+
+
+
+
+
+new_numbers=[2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50, 52, 54, 56, 58, 60, 62, 64, 66, 68, 70, 72, 74, 76, 78, 80, 82, 84, 86, 88, 90, 92, 94, 96, 98, 100, 102, 104, 106, 108, 110, 112, 114, 116, 118, 120, 122, 124, 126, 128, 130, 132, 134, 136, 138, 140, 142, 144, 146, 148, 150, 152, 154, 156, 158, 160, 162, 164, 166, 168, 170, 172, 174, 176, 178, 180, 182, 184, 186, 188, 190, 192, 194, 196, 198, 200, 202, 204, 206, 208, 210, 212, 214, 216, 218, 220, 222, 224, 226, 228, 230, 232, 234, 236, 238, 240, 242, 244, 246, 248, 250, 252, 254, 256, 258, 260, 262, 264, 266, 268, 270, 272, 274, 276, 278, 280, 282, 284, 286, 288, 290, 292, 294, 296, 298, 300, 302, 304, 306, 308, 310, 312, 314, 316, 318, 320, 322, 324, 326, 328, 330, 332, 334, 336, 338, 340, 342, 344, 346, 348, 350, 352, 354, 356, 358, 360, 362, 364, 366, 368, 370, 372, 374, 376, 378, 380, 382, 384, 386, 388, 390, 392, 394, 396, 398, 400, 402, 404, 406, 408, 410, 412, 414, 416, 418, 420, 422, 424, 426, 428, 430, 432, 434, 436, 438, 440, 442, 444, 446, 448, 450, 452, 454, 456, 458, 460, 462, 464, 466, 468, 470, 472, 474, 476, 478, 480, 482, 484, 486, 488, 490, 492, 494, 496, 498]
+
+newss=map(lambda x:x**2,new_numbers)
+print(list(newss))
